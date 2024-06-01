7d7c8cebd000f43d8ee0f032a71d39d0ed087c66
hixi4 <merezhkog07@gmail.com>
2024-06-02 01:57:04 +0300

    update

--- zoo1.go
+++ zoo1.go
@@ -21,6 +21,30 @@ type Cage struct {
 	Animals []Animal
 }
 
+// Функція для створення зоопарку зі звірами
+func createZoo() Zoo {
+	return Zoo{
+		Animals: []Animal{
+			{"Лев", 1},
+			{"Тигр", 2},
+			{"Мавпа", 3},
+			{"Змія", 4},
+			{"Слон", 5},
+		},
+	}
+}
+
+// Функція для створення кліток
+func createCages() []*Cage {
+	return []*Cage{
+		{ID: 1},
+		{ID: 2},
+		{ID: 3},
+		{ID: 4},
+		{ID: 5},
+	}
+}
+
 // Функція для повернення звірів в їх клітки
 func (z *Zoo) ReturnToCage(cages []*Cage) {
 	for _, animal := range z.Animals {
@@ -33,33 +57,8 @@ func (z *Zoo) ReturnToCage(cages []*Cage) {
 	}
 }
 
-func main() {
-	// Створюю звірів
-	zoo := Zoo{
-		Animals: []Animal{
-			{"Лев", 1},
-			{"Тигр", 2},
-			{"Мавпа", 3},
-			{"Змія", 4},
-			{"Слон", 5},
-		},
-	}
-	// Створюю клітки
-	cages := []*Cage{
-		{ID: 1},
-		{ID: 2},
-		{ID: 3},
-		{ID: 4},
-		{ID: 5},
-	}
-
-	// Ситуація, коли звірі втекли з кліток
-	fmt.Println("Звірі утекли з зоопарку!")
-
-	// Функція для повернення звірів в клітки
-	zoo.ReturnToCage(cages)
-
-	// Виводимо інформацію про клітки і їхніх мешканців
+// Функція для виведення інформації про клітки і їхніх мешканців
+func printCagesInfo(cages []*Cage) {
 	for _, cage := range cages {
 		fmt.Printf("Клітка %d містить звірів: ", cage.ID)
 		for _, animal := range cage.Animals {
@@ -67,4 +66,21 @@ func main() {
 		}
 		fmt.Println()
 	}
+}
+
+func main() {
+	// Створюємо зоопарк
+	zoo := createZoo()
+
+	// Створюємо клітки
+	cages := createCages()
+
+	// Ситуація, коли звірі втекли з кліток
+	fmt.Println("Звірі утекли з зоопарку!")
+
+	// Повернення звірів в клітки
+	zoo.ReturnToCage(cages)
+
+	// Виводимо інформацію про клітки і їхніх мешканців
+	printCagesInfo(cages)
 }
